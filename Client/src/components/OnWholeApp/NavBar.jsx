4996f5ffe444b314b87699b3c7fe8316094b30f7
Prashu7487 <pm7487912@gmail.com>
2024-08-07 20:03:02 +0530

    navbar temp changes

--- Client/src/components/OnWholeApp/NavBar.jsx
+++ Client/src/components/OnWholeApp/NavBar.jsx
@@ -4,7 +4,7 @@ import { useGlobalData } from "../../GlobalContext";
 
 const NavBar = function NavBar() {
   const { GlobalData, setGlobalData } = useGlobalData();
-  const clientName = GlobalData.Client.ClientName;
+  // const clientName = GlobalData.Client.ClientName;
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
   const handleNavLinkClick = () => {
@@ -12,84 +12,54 @@ const NavBar = function NavBar() {
   };
 
   return (
-    <nav className="navbar navbar-dark bg-dark fixed-top">
+    <nav
+      className="navbar navbar-expand-lg"
+      style={{ backgroundColor: "#e3f2fd" }}
+    >
       <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          FedClient
+        </a>
         <button
           className="navbar-toggler"
           type="button"
-          onClick={() => setIsNavbarOpen(!isNavbarOpen)} // Toggle the navbar state
-          aria-expanded={isNavbarOpen ? "true" : "false"}
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <a className="navbar-brand" href="#">
-          {clientName === "" ? "Unknown" : clientName}
-        </a>
-        <div
-          className={`offcanvas offcanvas-start text-bg-dark ${
-            isNavbarOpen ? "show" : ""
-          }`}
-          tabIndex="-1"
-          id="offcanvasDarkNavbar"
-        >
-          <div className="offcanvas-header">
-            <h5 className="offcanvas-title">Hello {clientName}</h5>
-            <button
-              type="button"
-              className="btn-close btn-close-white"
-              onClick={() => setIsNavbarOpen(false)} // Close the offcanvas navbar when close button is clicked
-            ></button>
-          </div>
-          <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-2">
-              <li className="nav-item">
-                <NavLink
-                  className={"nav-link"}
-                  to="/"
-                  onClick={handleNavLinkClick}
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  className={"nav-link"}
-                  to="/Register"
-                  onClick={handleNavLinkClick}
-                >
-                  Register
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  className={"nav-link"}
-                  to="/Request"
-                  onClick={handleNavLinkClick}
-                >
-                  Request
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink
-                  className={"nav-link"}
-                  to="/TrainingStatus"
-                  onClick={handleNavLinkClick}
-                >
-                  TrainingStatus
-                </NavLink>
-              </li>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className={"nav-link"} to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className={"nav-link"} to="/Register">
+                Register
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className={"nav-link"} to="/Request">
+                Request
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className={"nav-link"} to="/TrainingStatus">
+                TrainingStatus
+              </NavLink>
+            </li>
 
-              <li className="nav-item">
-                <NavLink
-                  className={"nav-link"}
-                  to="/Results"
-                  onClick={handleNavLinkClick}
-                >
-                  TrainingResults
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+            <li className="nav-item">
+              <NavLink className={"nav-link"} to="/Results">
+                TrainingResults
+              </NavLink>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
