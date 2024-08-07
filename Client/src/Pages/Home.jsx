8142b2262a3b25c5e73a8bcaab5b3207537dd890
Prashu7487 <pm7487912@gmail.com>
2024-08-08 03:25:16 +0530

    home page fixed

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import FedClientImage from "../assets/FedClient.jpeg";
+import FedprocessGif from "../assets/fedprocess.gif"; // card2
+import StepsGif from "../assets/steps.gif"; // in backgrd
 
 export default function Home() {
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-10 col-md-8 col-lg-6">
-          <div className="card shadow-sm">
+    <div className="container-fluid d-flex align-items-center">
+      <div className="row w-100">
+        <div className="col-md-5 d-none d-md-block">
+          <img src={StepsGif} alt="Steps" className="img-fluid" />
+        </div>
+        <div className="col-md-7 d-flex justify-content-center align-items-center">
+          <div className="card shadow-md mb-5 rounded card-hover">
             <div className="card-body">
               <div className="text-center mb-4">
                 <img
@@ -22,11 +27,10 @@ export default function Home() {
               <h1 className="text-center mb-4 text-primary">
                 Welcome to FedClient
               </h1>
-              <p className="text-center">
-                This is Client Application for a Federated Learning Architecture
-              </p>
               <div className="alert alert-info text-center" role="alert">
-                updates coming soon!
+                <strong>
+                  This is Client Application to simulate Federated Learning
+                </strong>
               </div>
             </div>
           </div>
@@ -34,4 +38,16 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+{
+  /* <div className="col-10 col-md-5 col-lg-4 mb-5" style={overlapStyle}>
+  <div className="card shadow-sm">
+    <div className="card-body">
+      <div className="text-center">
+        <img src={FedprocessGif} className="img-fluid" alt="Fedprocess Gif" />
+      </div>
+    </div>
+  </div>
+</div>; */
 }
