8e5cb4db8ee85bfd1a5736ae69565d52e754fead
Prashu7487 <pm7487912@gmail.com>
2024-08-07 23:17:56 +0530

    navbar fixed

--- Client/src/components/OnWholeApp/NavBar.jsx
+++ Client/src/components/OnWholeApp/NavBar.jsx
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { useGlobalData } from "../../GlobalContext";
 
-const NavBar = function NavBar() {
-  const { GlobalData, setGlobalData } = useGlobalData();
-  // const clientName = GlobalData.Client.ClientName;
+const NavBar = () => {
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
-  const handleNavLinkClick = () => {
-    setIsNavbarOpen(false); // Close the offcanvas navbar when a NavLink is clicked
+  const handleToggle = () => {
+    setIsNavbarOpen(!isNavbarOpen);
   };
 
   return (
     <nav
-      className="navbar navbar-expand-lg"
-      style={{ backgroundColor: "#e3f2fd" }}
+      className="navbar navbar-expand-lg bg-dark border-bottom border-body"
+      data-bs-theme="dark"
     >
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
@@ -23,40 +20,41 @@ const NavBar = function NavBar() {
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
+          onClick={handleToggle}
           aria-controls="navbarSupportedContent"
-          aria-expanded="false"
+          aria-expanded={isNavbarOpen ? "true" : "false"}
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink className={"nav-link"} to="/">
+        <div
+          className={`collapse navbar-collapse ${isNavbarOpen ? "show" : ""}`}
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item me-3">
+              <NavLink className="nav-link" to="/">
                 Home
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className={"nav-link"} to="/Register">
+            <li className="nav-item me-3">
+              <NavLink className="nav-link" to="/Register">
                 Register
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className={"nav-link"} to="/Request">
+            <li className="nav-item me-3">
+              <NavLink className="nav-link" to="/Request">
                 Request
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className={"nav-link"} to="/TrainingStatus">
-                TrainingStatus
+            <li className="nav-item me-3">
+              <NavLink className="nav-link" to="/TrainingStatus">
+                Training Status
               </NavLink>
             </li>
-
-            <li className="nav-item">
-              <NavLink className={"nav-link"} to="/Results">
-                TrainingResults
+            <li className="nav-item me-3">
+              <NavLink className="nav-link" to="/Results">
+                Training Results
               </NavLink>
             </li>
           </ul>
